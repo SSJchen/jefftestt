ff7ec65a0a565bfcd47fd08de890f685c97297b5
SSJchen <ya.hoo@me.com>
2016-07-11 17:39:09 +0800

    sparkR

--- a.R
+++ a.R
@@ -1,3 +1,170 @@
-print("hello")
+#安装
+#install.packages("devtools")
+#devtools::install_github("rstudio/sparklyr")
 
-print("ok")
+#library(sparklyr)
+#spark_install(version = "1.6.1")
+
+#连接Spark
+library(sparklyr)
+library(dplyr)
+sc <- spark_connect(master = "local")
+
+#http://spark.rstudio.com/deployment.html
+
+
+#读取数据
+#用copy_to函数可以实现将R中的数据框导入到Spark。下面我将R自带的iris数据集，nycflights13包的flights数据集，以及Lahman包的Batting数据集复制到Spark（请确保安装了这两个包）。
+
+iris_tbl <- copy_to(sc, iris)
+flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
+batting_tbl <- copy_to(sc, Lahman::Batting, "batting")
+
+#使用dplyr的src_tbls函数可以列出所有可用的表
+src_tbls(sc)
+
+#使用dplyr语法
+# 筛选出飞机晚点两分钟的航班信息
+flights_tbl %>% filter(dep_delay == 2)
+
+#航班延误信息的数据可视化：
+delay <- flights_tbl %>% 
+  group_by(tailnum) %>%
+  summarise(count = n(), dist = mean(distance), delay = mean(arr_delay)) %>%
+  filter(count > 20, dist < 2000, !is.na(delay)) %>%
+  collect
+
+# 绘图
+library(ggplot2)
+ggplot(delay, aes(dist, delay)) +
+  geom_point(aes(size = count), alpha = 1/2) +
+  geom_smooth() +
+  scale_size_area(max_size = 2)
+
+
+#窗口函数
+batting_tbl %>%
+  select(playerID, yearID, teamID, G, AB:H) %>%
+  arrange(playerID, yearID, teamID) %>%
+  group_by(playerID) %>%
+  filter(min_rank(desc(H)) <= 2 & H > 0)
+
+#调用MLlib
+#線性回歸
+# 将mtcar数据集复制到spark
+mtcars_tbl <- copy_to(sc, mtcars)
+
+# 先对数据做变换，然后将数据集分割为训练集和测试集
+partitions <- mtcars_tbl %>%
+  filter(hp >= 100) %>%
+  mutate(cyl8 = cyl == 8) %>%
+  sdf_partition(training = 0.5, test = 0.5, seed = 1099)
+
+# 对训练数据集做模型拟合
+fit <- partitions$training %>%
+  ml_linear_regression(response = "mpg", features = c("wt", "cyl"))
+
+#使用summary()函数可以查看模型的拟合效果以及每个预测指标的统计意义
+summary(fit)
+
+#Linear Regression
+lm_model <- iris_tbl %>%
+  select(Petal_Width, Petal_Length) %>%
+  ml_linear_regression(response = "Petal_Length", features = c("Petal_Width"))
+
+iris_tbl %>%
+  select(Petal_Width, Petal_Length) %>%
+  collect %>%
+  ggplot(aes(Petal_Length, Petal_Width)) +
+  geom_point(aes(Petal_Width, Petal_Length), size = 2, alpha = 0.5) +
+  geom_abline(aes(slope = coef(lm_model)[["Petal_Width"]],
+                  intercept = coef(lm_model)[["(Intercept)"]]),
+              color = "red") +
+  labs(
+    x = "Petal Width",
+    y = "Petal Length",
+    title = "Linear Regression: Petal Length ~ Petal Width",
+    subtitle = "Use Spark.ML linear regression to predict petal length as a function of petal width."
+  )
+
+
+
+#K-Means Clustering
+kmeans_model <- iris_tbl %>%
+  select(Petal_Width, Petal_Length) %>%
+  ml_kmeans(centers = 3)
+
+# print our model fit
+print(kmeans_model)
+
+# predict the associated class
+predicted <- predict(kmeans_model)
+table(predicted, iris$Species)
+
+# plot cluster membership
+sdf_predict(kmeans_model) %>%
+  collect() %>%
+  ggplot(aes(Petal_Length, Petal_Width)) +
+  geom_point(aes(Petal_Width, Petal_Length, col = factor(prediction + 1)),
+             size = 2, alpha = 0.5) + 
+  geom_point(data = kmeans_model$centers, aes(Petal_Width, Petal_Length),
+             col = scales::muted(c("red", "green", "blue")),
+             pch = 'x', size = 12) +
+  scale_color_discrete(name = "Predicted Cluster",
+                       labels = paste("Cluster", 1:3)) +
+  labs(
+    x = "Petal Length",
+    y = "Petal Width",
+    title = "K-Means Clustering",
+    subtitle = "Use Spark.ML to predict cluster membership with the iris dataset."
+  )
+
+
+#Logistic Regression
+# Prepare beaver dataset
+beaver <- beaver2
+beaver$activ <- factor(beaver$activ, labels = c("Non-Active", "Active"))
+copy_to(sc, beaver, "beaver")
+beaver_tbl <- tbl(sc, "beaver")
+
+glm_model <- beaver_tbl %>%
+  mutate(response = as.numeric(activ == "Active")) %>%
+  ml_logistic_regression(response = "response", features = "temp")
+
+print(glm_model)
+
+
+#Partitioning
+#Split a Spark DataFrame into training, test datasets.
+partitions <- tbl(sc, "iris") %>%
+  sdf_partition(training = 0.75, test = 0.25, seed = 1099)
+
+fit <- partitions$training %>%
+  ml_linear_regression(response = "Petal_Length", features = c("Petal_Width"))
+
+head(predict(fit, partitions$test))
+
+
+#Principal Components Analysis
+pca_model <- tbl(sc, "iris") %>%
+  select(-Species) %>%
+  ml_pca()
+print(pca_model)
+
+
+#Random Forest
+rf_model <- iris_tbl %>%
+  ml_random_forest(
+    response = "Species",
+    features = c("Petal_Length", "Petal_Width"),
+    max.bins = 32L,
+    max.depth = 5L,
+    num.trees = 20L
+  )
+
+rf_predict <- predict(rf_model, iris_tbl)
+iris_fit <- iris_tbl %>%
+  select(Species) %>%
+  collect %>%
+  mutate(fit = rf_predict)
+table(iris_fit)
